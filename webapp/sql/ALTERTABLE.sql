893910db72a8507ca1c1e712d3372f9fcdaad6c3
pikachu0310 <pikachu13711@gmail.com>
2023-11-25 11:06:44 +0900

    場所間違えた

--- webapp/sql/ALTERTABLE.sql
+++ webapp/sql/ALTERTABLE.sql
@@ -2,5 +2,7 @@ ALTER TABLE `isupipe`.`livestream_tags` ADD INDEX `livestream_id` (`livestream_i
 ALTER TABLE `isupipe`.`icons` ADD INDEX `user_id` (`user_id`);
 ALTER TABLE `isupipe`.`livecomments` ADD INDEX `livestream_id` (`livestream_id`);
 ALTER TABLE `isupipe`.`ng_words` ADD INDEX `user_id_livestream_id` (`user_id`, `livestream_id`);
-
 ALTER TABLE `isudns`.`records` ADD INDEX (name);
+
+
+
